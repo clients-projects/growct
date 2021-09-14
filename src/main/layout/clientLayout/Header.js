e7e8663187d9df1469035da37defdfebb4d1e83a
Igboanugwo Collins <munisco12@gmail.com>
2021-09-14 07:13:34 +0100

    working on homepage

--- src/main/layout/clientLayout/Header.js
+++ src/main/layout/clientLayout/Header.js
@@ -34,11 +34,11 @@ function Header(props) {
                                         alt='logo'
                                         style={{ height: '70px' }}
                                     />
-                                    Robot 44 
+                                    Robot 44 Trade
                                     <a href='/'>main</a>
                                 </div>
                             </div>
-                            <div class='lang tCell right'>
+                            {/* <div class='lang tCell right'>
                                 <Link
                                     to='#'
                                     style={{
@@ -51,7 +51,7 @@ function Header(props) {
                                         backgroundImage: `url(${Lang2})`,
                                     }}
                                 ></Link>
-                            </div>
+                            </div> */}
 
                             <div className='clock tCell right'>
                                 <span className='time gradTxt' id='time'>
