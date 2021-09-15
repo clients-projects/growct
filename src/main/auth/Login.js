a41e0326f69e443255c642e13802d21c143f8346
Igboanugwo Collins <munisco12@gmail.com>
2021-09-15 07:23:45 +0100

    working on login

--- src/main/auth/Login.js
+++ src/main/auth/Login.js
@@ -98,56 +98,97 @@ const Login = (props) => {
     return (
         <>
             ============
-             <section className="loginpage">
-  <div className="inside_inner">
- 
-    <div className="loginwrappers">
-      <div className="login_left">
-		  
-		 <div className="login-top">
-			   <h2><a href="?a=home" id="logo"></a>
-			    <a href="?a=home">Back to homepage</a>
-			   </h2>
-			  
-		 </div> 
-		  
-        <div className="form-container login">
-			
-          <h3>Member Login</h3>
-<form>
+            <section className='loginpage'>
+                <div className='inside_inner'>
+                    <div className='loginwrappers'>
+                        <div className='login_left'>
+                            <div className='login-top'>
+                                <h2>
+                                    <a href='?a=home' id='logo'></a>
+                                    <a href='?a=home'>Back to homepage</a>
+                                </h2>
+                            </div>
 
-<table width="100%" border='0' cellPadding='4' cellSpacing='4'>
-<tr>
- <td colspan="2"><input type='text' name='username' value='' className='inpts' size='30' autofocus="autofocus" placeholder="Username"/></td>
-</tr><tr>
- <td colspan="2"><input type='password' name='password' value='' className='inpts' size='30' placeholder="Password"/></td>
-</tr>
-<tr>
-                <td colspan="2"></td>
-              </tr>
-                            <tr>
-                <td height="40" valign="middle"><span className="bunicon">
-                    <input type=submit value="Login" className=sbmt></span></td>
-                <td align="right" valign="middle"><a href="?a=forgot_password" className="small_text">Forgot your password?</a></td>
-              </tr>
-              </table>
-</form>
+                            <div className='form-container login'>
+                                <h3>Member Login</h3>
+                                <form>
+                                    <table
+                                        width='100%'
+                                        border='0'
+                                        cellPadding='4'
+                                        cellSpacing='4'
+                                    >
+                                        <tr>
+                                            <td colspan='2'>
+                                                <input
+                                                    type='text'
+                                                    name='username'
+                                                    value=''
+                                                    className='inpts'
+                                                    size='30'
+                                                    autofocus='autofocus'
+                                                    placeholder='Username'
+                                                />
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td colspan='2'>
+                                                <input
+                                                    type='password'
+                                                    name='password'
+                                                    value=''
+                                                    className='inpts'
+                                                    size='30'
+                                                    placeholder='Password'
+                                                />
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td colspan='2'></td>
+                                        </tr>
+                                        <tr>
+                                            <td height='40' valign='middle'>
+                                                <span className='bunicon'>
+                                                    <input
+                                                        type='submit'
+                                                        value='Login'
+                                                        className='sbmt'
+                                                    />
+                                                </span>
+                                            </td>
+                                            <td align='right' valign='middle'>
+                                                <a
+                                                    href='?a=forgot_password'
+                                                    className='small_text'
+                                                >
+                                                    Forgot your password?
+                                                </a>
+                                            </td>
+                                        </tr>
+                                    </table>
+                                </form>
 
-          <div className="loginyellowbottom">
-			<a href="?a=signup" className="loginnewuser">New User? <span>SignUp Here</span></a>
-			</div>
-          
-        </div>
-      </div>
-    </div>
-  </div>
-</section>
-
-<section className="footerContainer loginfooter">
-  <div className="footerInner">
-<p>Copyright &copy; 2020. <a href="?a=home"></a> All Rights Reserved</p>
-  </div>
-</section>
+                                <div className='loginyellowbottom'>
+                                    <a
+                                        href='?a=signup'
+                                        className='loginnewuser'
+                                    >
+                                        New User? <span>SignUp Here</span>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section className='footerContainer loginfooter'>
+                <div className='footerInner'>
+                    <p>
+                        Copyright &copy; 2020. <a href='?a=home'></a> All Rights
+                        Reserved
+                    </p>
+                </div>
+            </section>
             ===========
             <div class='bodyWrap'>
                 <div
@@ -160,7 +201,7 @@ const Login = (props) => {
                         <div class='head'>
                             <div class='logo invisLink' style={{ opacity: 0 }}>
                                 <img src={Logo} alt='logo' height='70px;' />
-                                
+
                                 <Link to='/'>main</Link>
                             </div>
                         </div>
