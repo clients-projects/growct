5d95b4d3a0159cf992cf5ff714aa3568e7a07c3c
Igboanugwo Collins <munisco12@gmail.com>
2021-10-01 11:48:45 +0100

    working on auth

--- src/containers/ContactUs.js
+++ src/containers/ContactUs.js
@@ -91,7 +91,7 @@ const ContactUs = (props) => {
                             <div className='map' id='map'>
                                 <iframe
                                     title='iframe2'
-                                    src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2479.664190550833!2d-0.0020082838559824082!3d51.57438937964627!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47d8a761dc8a2877%3A0x445ae65e3d837263!2s23+Sandringham+Rd%2C+London!5e0!3m2!1suk!2suk!4v1533728208403'
+                                    src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2479.664190550833!2d-0.0020082838559824082!3d51.57438937964627!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47d8a761dc8a2877%3A0x445ae65e3d837263!2s23+Irvine%2C+Alberta!5e0!3m2!1suk!2suk!4v1533728208403'
                                     width='100%'
                                     height='100%'
                                     frameborder='0'
