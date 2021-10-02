879cb9f0501c94faac6290db714fc2e84dc1f321
Igboanugwo Collins <munisco12@gmail.com>
2021-10-02 22:06:12 +0100

    updated user deposit history

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import connect from 'react-redux'
+import {connect} from 'react-redux'
 
 import BannerThumb from '../sass/images/bannerthumb.png'
 import GeoTrust from '../sass/images/geotrust.png'
