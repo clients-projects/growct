ec2e1acc6a18fdab8be73bb3096512c1d63c03e4
Igboanugwo Collins <munisco12@gmail.com>
2021-10-02 21:09:49 +0100

    updated user deposit history

--- src/main/layout/clientLayout/Transactions.jsx
+++ src/main/layout/clientLayout/Transactions.jsx
@@ -34,7 +34,7 @@ function Transactions(props) {
         }
 
         if (getDepositHistory.length > 0) {
-            console.log({getDepositHistoy})
+            console.log({getDepositHistory})
             for (let i of getDepositHistory) {
                 if (i.planName === 'Helm') {
                     setHelm(i.amount)
--- src/store/actions/Auth.js
+++ src/store/actions/Auth.js
@@ -1,8 +1,8 @@
 import * as actions from './actionTypes'
 
-//const URL = 'http://localhost:3030'
+const URL = 'http://localhost:3030'
 
-const URL = 'https://growct.herokuapp.com'
+//const URL = 'https://growct.herokuapp.com'
 
 export const authStart = () => {
     console.log('the url', URL)
