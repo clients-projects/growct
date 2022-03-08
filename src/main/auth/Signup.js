b7a4432a120b0cf338d1a5b124c362b2afd8340d
Igboanugwo Collins <munisco12@gmail.com>
2022-03-08 19:55:25 +0100

    updated

--- src/main/auth/Signup.js
+++ src/main/auth/Signup.js
@@ -10,7 +10,7 @@ import Auth from './Auth'
 
 const Signup = (props) => {
     const parsed = queryString.parse(window.location.search)
-    console.log("update")
+    console.log("update!!")
 
     const [state, setState] = useState({
         signupForm: {
